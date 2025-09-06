3a3db6db4ecad11200c783007252ed7908fc6114
cantaruttim <cantaruttim@outlook.com>
2025-09-06 11:45:22 -0300

    migrations

---
+++ atividades/src/main/resources/db/migration/V1__Create_Table_Activities.sql
@@ -0,0 +1,10 @@
+create table if not exists `activities` (
+    `id` bigint(20) not null auto_increment,
+    `name` varchar(100) not null,
+    `email` varchar(100) not null,
+    `registrationNumber` varchar(100) not null,
+    `module` varchar(100) not null,
+    `questionOne` varchar(400) not null,
+    `questionTwo` varchar(400) not null,
+        primary key (`id`)
+)
---
+++ atividades/src/main/resources/db/migration/V2__Insert_Into_Activities.sql
@@ -0,0 +1,17 @@
+insert into `activities`
+    (`id`,`name`,`email`,`registrationNumber`,`module`,`questionOne`,`questionTwo`)
+    values
+    (1,
+        "Matheus de Almeida Cantarutti",
+        "cantaruttim@outlook.com",
+        "2025025898",
+        "A poesia é a arte de transformar emoções em palavras, criando imagens que tocam a alma. Ela revela sentimentos profundos, conecta pessoas e oferece novas formas de ver o mundo. Com ritmo e beleza, a poesia é um refúgio para a imaginação e um convite à reflexão sobre a vida.",
+        "A poesia é a arte de transformar emoções em palavras, criando imagens que tocam a alma. Ela revela sentimentos profundos, conecta pessoas e oferece novas formas de ver o mundo. Com ritmo e beleza, a poesia é um refúgio para a imaginação e um convite à reflexão sobre a vida."
+    ),
+    (2,
+        "Gabriella Quinteiro Ferreira",
+        "gabriella.quinteiro@hotmail.com",
+        "20205024272"
+        "A poesia é a arte de transformar emoções em palavras, criando imagens que tocam a alma. Ela revela sentimentos profundos, conecta pessoas e oferece novas formas de ver o mundo. Com ritmo e beleza, a poesia é um refúgio para a imaginação e um convite à reflexão sobre a vida.",
+        "A poesia é a arte de transformar emoções em palavras, criando imagens que tocam a alma. Ela revela sentimentos profundos, conecta pessoas e oferece novas formas de ver o mundo. Com ritmo e beleza, a poesia é um refúgio para a imaginação e um convite à reflexão sobre a vida."
+    );
