89ea4a127e9f3a24d336375d2e35b8aab79019d6
cantaruttim <cantaruttim@outlook.com>
2025-09-06 15:05:05 -0300

    data sent implemented

--- atividades/src/main/resources/db/migration/V1__Create_Table_Activities.sql
+++ atividades/src/main/resources/db/migration/V1__Create_Table_Activities.sql
@@ -6,5 +6,6 @@ create table if not exists `activities` (
     `module` varchar(100) not null,
     `questionOne` varchar(400) not null,
     `questionTwo` varchar(400) not null,
+    `sentAt` timestamp not null,
         primary key (`id`)
 )
--- atividades/src/main/resources/db/migration/V2__Insert_Into_Activities.sql
+++ atividades/src/main/resources/db/migration/V2__Insert_Into_Activities.sql
@@ -1,4 +1,4 @@
-INSERT INTO `activities` (`id`, `name`, `email`, `registrationNumber`, `module`, `questionOne`, `questionTwo`)
+INSERT INTO `activities` (`id`, `name`, `email`, `registrationNumber`, `module`, `questionOne`, `questionTwo`, `sentAt`)
 VALUES
     (1,
         'Matheus de Almeida Cantarutti',
@@ -6,7 +6,8 @@ VALUES
         '2025025898',
         'Maturidade Espiritual',
         'A poesia é a arte de transformar emoções em palavras, criando imagens que tocam a alma. Ela revela sentimentos profundos, conecta pessoas e oferece novas formas de ver o mundo. Com ritmo e beleza, a poesia é um refúgio para a imaginação e um convite à reflexão sobre a vida.',
-        'A poesia é a arte de transformar emoções em palavras, criando imagens que tocam a alma. Ela revela sentimentos profundos, conecta pessoas e oferece novas formas de ver o mundo. Com ritmo e beleza, a poesia é um refúgio para a imaginação e um convite à reflexão sobre a vida.'
+        'A poesia é a arte de transformar emoções em palavras, criando imagens que tocam a alma. Ela revela sentimentos profundos, conecta pessoas e oferece novas formas de ver o mundo. Com ritmo e beleza, a poesia é um refúgio para a imaginação e um convite à reflexão sobre a vida.',
+        now()
     ),
     (2,
         'Gabriella Quinteiro Ferreira',
@@ -14,5 +15,6 @@ VALUES
         '20205024272',
         'Maturidade Espiritual',
         'A poesia é a arte de transformar emoções em palavras, criando imagens que tocam a alma. Ela revela sentimentos profundos, conecta pessoas e oferece novas formas de ver o mundo. Com ritmo e beleza, a poesia é um refúgio para a imaginação e um convite à reflexão sobre a vida.',
-        'A poesia é a arte de transformar emoções em palavras, criando imagens que tocam a alma. Ela revela sentimentos profundos, conecta pessoas e oferece novas formas de ver o mundo. Com ritmo e beleza, a poesia é um refúgio para a imaginação e um convite à reflexão sobre a vida.'
+        'A poesia é a arte de transformar emoções em palavras, criando imagens que tocam a alma. Ela revela sentimentos profundos, conecta pessoas e oferece novas formas de ver o mundo. Com ritmo e beleza, a poesia é um refúgio para a imaginação e um convite à reflexão sobre a vida.',
+        now()
     );
